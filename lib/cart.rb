6f06e42dfb560e7878269e44fea1d235681c2701
Ace <waynn7788@gmail.com>
2018-04-25 15:33:02 +0800

    finish test of 5% off of 2nd example

--- lib/cart.rb
+++ lib/cart.rb
@@ -10,7 +10,12 @@ class Cart
     @ordered_items = @ordered_items.delete_if{ |i| i == 0 }
 
     if @ordered_items.size == 2
-      total += 2*PRICE*0.95
+      while @ordered_items.size > 0
+        total += 2*PRICE*0.95
+        @ordered_items[0] -= 1
+        @ordered_items[1] -= 1
+        @ordered_items = @ordered_items.delete_if{ |i| i == 0 }
+      end
     else
       for item in @ordered_items
         total += item*PRICE
